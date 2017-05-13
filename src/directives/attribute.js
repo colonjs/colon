9099ebf81fe66460b0c630011ba60ed664eb95b6
JustClear <576839360@qq.com>
2017-05-13 22:58:49 +0800

    ✨ add directive `:attribute`

---
+++ src/directives/attribute.js
@@ -0,0 +1,5 @@
+export default {
+    update(value) {
+        this.node.setAttribute(this.attrName, value);
+    },
+};
--- src/directives/index.js
+++ src/directives/index.js
@@ -1,9 +1,11 @@
 import src from './src';
 import show from './show';
 import text from './text';
+import attribute from './attribute';
 
 export default {
     src,
     show,
     text,
+    attribute,
 };
