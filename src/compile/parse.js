9cf0e85a365572e61a6b922c7c41e3916142e645
大板栗 <576839360@qq.com>
2017-07-28 17:13:45 +0800

    🐛 fix: expression parsing exception

--- src/compile/parse.js
+++ src/compile/parse.js
@@ -4,24 +4,21 @@ export default {
     text(text) {
         if (!tagRE.test(text)) return text;
 
-        let segments = [],
-            expression,
-            index,
-            matched,
-            nextIndex = 0;
-
-        tagRE.lastIndex = 0;
+        const tokens = [];
+        let lastIndex = tagRE.lastIndex = 0;
+        let index, matched;
 
         while (matched = tagRE.exec(text)) {
             index = matched.index;
-            if (index > nextIndex) segments.push(`"${text.slice(nextIndex, index)}"`);
-            expression = matched[1].trim();
-            segments.push(expression);
-            nextIndex = index + matched[0].length;
+            if (index > lastIndex) {
+                tokens.push(JSON.stringify(text.slice(lastIndex, index)));
+            }
+            tokens.push(matched[1].trim());
+            lastIndex = index + matched[0].length;
         }
 
-        if (nextIndex < text.length - 1) segments.push(text.slice(nextIndex));
+        if (lastIndex < text.length) tokens.push(JSON.stringify(text.slice(lastIndex)));
 
-        return segments.join('+');
+        return tokens.join('+');
     },
 };
